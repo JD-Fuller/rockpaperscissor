2c4176455b93ef45abb95e44c23e723c9869442b
JD-Fuller <jdfuller@live.com>
2019-11-13 14:05:22 -0700

    growing if else statements

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
+let selections = [
+  {
+    type: "Rock",
+    status: "Tie Ball Game!"
+  },
+  {
+    type: "Paper",
+    status: "Winner, Winner chicken dinner!"
+  },
+  {
+    type: "Scissor",
+    status: "You Lose! Ha-ha!"
+  }
+];
 
-/**
- * 
- * @param {string} playerChoice 
- */
-function play(playerChoice){
-let computerChoice = 'rock';
 /**
  * IF playerChoice == computerChoice { Then "Tie"}
- * 
+ *
  * IF playerChoice == Scissor { Then "You Lose"}
- * 
+ *
  * IF playerChoice == Paper { Then "You Win"}
  */
-if (playerChoice == computerChoice) 
-{return "Tie!"} 
-else if (playerChoice == 'scissor') 
-{document.querySelector('#gameResult')}
-else if (playerChoice == 'paper')
-{return "You Win!"}
-}
+/**
+ *
+ * @param {string} playerChoice
+ */
+function play(playerChoice) {
+  let computerChoice = "rock";
 
+  let results = selections.find(
+    selection => selection.type.toLowerCase() == playerChoice.toLowerCase()
+  );
+
+  document.querySelector("#gameResult").innerHTML = `<p>${results.status}`;
+
+
+
+  
+//   if (playerChoice.toLowerCase() == computerChoice) {
+//     document.querySelector("#gameResult").innerHTML = `
+// <p>${results.status}</p>`;
+//   } else if (playerChoice.toLowerCase() == "scissor") {
+//     document.querySelector("#gameResult").innerHTML = `
+// <p>${results.lose}</p>`;
+//   } else if (playerChoice.toLowerCase() == "paper") {
+//     document.querySelector("#gameResult").innerHTML = `
+// <p>${results.win}</p>`;
+//   }
+}
