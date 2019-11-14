d3f6327c33ca8f271c2a2883f85f3e8a2ef56ee2
JD-Fuller <jdfuller@live.com>
2019-11-13 20:03:15 -0700

    last commit before heading home

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-// let selections = [
-//   {type: "Rock",
-//   status: "Tie Ball Game!"},
-//   {type: "Paper",
-//   status: "Winner, Winner chicken dinner!"},
-//   {type: "Scissor",
-//   status: "You Lose! Ha-ha!"}
-// ];
-
-
-// let selections = [
-//   {tie: "Tie Ball Game!",
-//   win: "Winner, winner chicken dinner!",
-//   lose: "You lose! Go home ya loser!"
-//   }
-// ]
 
 let selections = [
   {type: "Rock",
@@ -38,6 +22,12 @@ let selections = [
 // // { type: "Batman"},
 // // { type: "Lizard"}
 // ]
+var score = {
+  wins: 0, 
+  ties: 0, 
+  losses: 0,
+  add: function(event){if(event = "win") { this.wins += 1;} else if (event = "lose") {this.losses +=1} else {this.ties +=1;}}
+}
 
 let computerSelection = [
   "rock", "paper", "scissor"
@@ -47,47 +37,49 @@ let computerSelection = [
  * @param {string} playerChoice
  */
 function play(playerChoice) {
-  // let computerChoice = "rock";
+ 
+  //RANDOM ComputerChoice Generator
 let computerChoice = computerSelection[Math.floor(Math.random()*computerSelection.length)];
   let results = selections.find(
     s => s.type.toLowerCase() == playerChoice.toLowerCase()
   );
-  // This code works for basic, canned response
-  // document.querySelector("#gameResult").innerHTML = `
-  // <h4>${results.type}</h4>
-  // <p>${results.status}</p>`;
 
-
+  //Result Selections
   if (playerChoice.toLowerCase() == computerChoice) {
     document.querySelector("#gameResult").innerHTML = `
+    <h4>${results.type}</h4>
 <p>${results.tie}</p>`;
-
+    score.ties++
   } else if (playerChoice.toLowerCase() == "scissor" && computerChoice == "rock") {
     document.querySelector("#gameResult").innerHTML = `
+    <h4>${results.type}</h4>
 <p>${results.lose}</p>`;
 }
 else if (playerChoice.toLowerCase() == "scissor" && computerChoice == "paper") {
  document.querySelector("#gameResult").innerHTML = `
+ <h4>${results.type}</h4>
 <p>${results.win}</p>`;
 }
  else if (playerChoice.toLowerCase() == "paper" && computerChoice == "rock") {
   document.querySelector("#gameResult").innerHTML = `
+  <h4>${results.type}</h4>
 <p>${results.win}</p>`;
 }
 else if (playerChoice.toLowerCase() == "paper" && computerChoice == "scissor") {
     document.querySelector("#gameResult").innerHTML = `
+    <h4>${results.type}</h4>
 <p>${results.lose}</p>`;
 }
 else if (playerChoice.toLowerCase() == "rock" && computerChoice == "paper") {
  document.querySelector("#gameResult").innerHTML = `
+ <h4>${results.type}</h4>
 <p>${results.lose}</p>`;
 }
 else if (playerChoice.toLowerCase() == "rock" && computerChoice == "scissor") {
   document.querySelector("#gameResult").innerHTML = `
+  <h4>${results.type}</h4>
  <p>${results.win}</p>`;
  }
-
-
 }
 
 
@@ -101,3 +93,28 @@ else if (playerChoice.toLowerCase() == "rock" && computerChoice == "scissor") {
 //  *
 //  * IF playerChoice == Paper { Then "You Win"}
 //  */
+
+
+
+  // This code works for basic, canned response
+  // document.querySelector("#gameResult").innerHTML = `
+  // <h4>${results.type}</h4>
+  // <p>${results.status}</p>`;
+
+
+// let selections = [
+//   {type: "Rock",
+//   status: "Tie Ball Game!"},
+//   {type: "Paper",
+//   status: "Winner, Winner chicken dinner!"},
+//   {type: "Scissor",
+//   status: "You Lose! Ha-ha!"}
+// ];
+
+
+// let selections = [
+//   {tie: "Tie Ball Game!",
+//   win: "Winner, winner chicken dinner!",
+//   lose: "You lose! Go home ya loser!"
+//   }
+// ]
